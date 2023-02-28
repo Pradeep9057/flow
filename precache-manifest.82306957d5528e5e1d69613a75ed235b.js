d68b639ba28024a4dfae2cefa9a1cdbe27aea000
Manish Singh <pradeepkumar.in9057@gmail.com>
2023-03-01 01:15:29 +0530

    Updates

---
+++ precache-manifest.82306957d5528e5e1d69613a75ed235b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b3cd0a5c620a9adfcd1",
+    "url": "/static/css/main.82c61bd8.chunk.css"
+  },
+  {
+    "revision": "1b3cd0a5c620a9adfcd1",
+    "url": "/static/js/main.1b3cd0a5.chunk.js"
+  },
+  {
+    "revision": "e72e18cde8be384a4da2",
+    "url": "/static/js/1.e72e18cd.chunk.js"
+  },
+  {
+    "revision": "2592ccf0523d601aa1ab",
+    "url": "/static/css/2.8cc4f8c8.chunk.css"
+  },
+  {
+    "revision": "2592ccf0523d601aa1ab",
+    "url": "/static/js/2.2592ccf0.chunk.js"
+  },
+  {
+    "revision": "0927513987a0e7766f83",
+    "url": "/static/js/runtime~main.09275139.js"
+  },
+  {
+    "revision": "d2417d9e0998dd3c7e833d6b5abe7901",
+    "url": "/static/media/img2.d2417d9e.jpg"
+  },
+  {
+    "revision": "d0651ed76bac20d73e9d3a0dded8c9c7",
+    "url": "/index.html"
+  }
+];
